d0c71c208c0149058ec6814077b047378c58730a
Nikolai Kasev <n.kasev98@gmail.com>
2020-05-05 17:02:47 +0300

    Refactored show_movie_queries

--- constants/queries_show_movie_by_id_and_date.py
+++ constants/queries_show_movie_by_id_and_date.py
@@ -1,14 +1,14 @@
-QUERY_SHOW_MOVIE_BY_ID_AND_DATE = '''SELECT name, Projections.id, date, time,type
+QUERY_SHOW_MOVIE_BY_ID_AND_DATE = '''SELECT name, Projections.id, time, type
                                      FROM Movies
                                      JOIN Projections ON Projections.movie_id = Movies.id
                                      WHERE movie_id = ?
                                      AND date = ?
-                                     ORDER BY date;
+                                     ORDER BY time;
                                      '''
 
-QUERY_SHOW_MOVIE_BY_ID = '''SELECT name, Projections.id, time, type
+QUERY_SHOW_MOVIE_BY_ID = '''SELECT name, Projections.id, time, date, type
                             FROM Movies
                             JOIN Projections ON Projections.movie_id = Movies.id
                             WHERE movie_id = ?
-                            ORDER BY time;
+                            ORDER BY date;
                             '''
