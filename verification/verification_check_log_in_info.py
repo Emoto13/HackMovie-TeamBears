ab5f77c316826946a565df3a8e459409f27f4b33
Nikolai Kasev <n.kasev98@gmail.com>
2020-05-05 19:51:55 +0300

    Add verficiation for log in info

---
+++ verification/verification_check_log_in_info.py
@@ -0,0 +1,7 @@
+from gateway.gateway_check_log_in_info import check_log_in_info_gateway
+
+
+def check_log_in_info(login_info):
+    info = check_log_in_info_gateway(login_info)
+    if len(info) == 0:
+        raise ValueError('Wrong name or password')
