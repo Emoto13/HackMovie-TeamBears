a5c8c644837639612c39c7a89bbef120a3a0a31c
Emoto13 <emcho_spassov@abv.bg>
2020-05-05 16:13:47 +0300

    Rename and fix typos

--- commands/show_movies_command.py
+++ commands/show_movies_command.py
@@ -1,10 +1,9 @@
-from gateway.show_movies_gateway import show_movies_sorted
+from gateway.gateway_show_movies import show_movies_sorted
 
 
 def show_movies_command():
     sorted_movies = show_movies_sorted()
+    # move to templates later
     print("Current movies are:")
-    movie_id = 1
     for movie in sorted_movies:
-        print(f'[{movie_id}] - {movie[1]} ({movie[2]})')
-        movie_id += 1
+        print(f'[{movie[0]}] - {movie[1]} ({movie[2]})')
--- constants/queries_initial_database.py
+++ constants/queries_initial_database.py
@@ -1,16 +1,16 @@
-movie_query_init = '''CREATE TABLE IF NOT EXISTS Movies
+QUERY_MOVIE_INIT = '''CREATE TABLE IF NOT EXISTS Movies
     (id integer primary key AUTOINCREMENT unique not NULL,
     name varchar(100) not NULL,
     rating real not NULL);'''
 
-movie_query_fill = '''INSERT INTO Movies (name, rating)
+QUERY_MOVIE_FILL = '''INSERT INTO Movies (name, rating)
                         VALUES
                         ("The Hunger Games: Catching Fire", 7.9),
                         ("Wreck-It Ralph", 7.8),
                         ("Her", 8.3);'''
 
 
-projections_query_init = '''CREATE TABLE IF NOT EXISTS Projections
+QUERY_PROJECTIONS_INIT = '''CREATE TABLE IF NOT EXISTS Projections
     (id integer primary key AUTOINCREMENT unique not NULL,
     movie_id integer not NULL,
     type varchar(5) not NULL,
@@ -18,7 +18,7 @@ projections_query_init = '''CREATE TABLE IF NOT EXISTS Projections
     time varchar(10) not NULL,
     FOREIGN KEY (movie_id) REFERENCES Movies(id));'''
 
-projections_query_fill = '''INSERT INTO Projections (movie_id, type, date, time)
+QUERY_PROJECTIONS_FILL = '''INSERT INTO Projections (movie_id, type, date, time)
                                 VALUES
                                 (1, "3D", "2020-04-01", "19:10"),
                                 (1, "2D", "2020-04-01", "19:00"),
@@ -27,19 +27,19 @@ projections_query_fill = '''INSERT INTO Projections (movie_id, type, date, time)
                                 (2, "3D", "2020-04-02", "22:00"),
                                 (2, "2D", "2020-04-02", "19:30");'''
 
-users_query_init = '''CREATE TABLE IF NOT EXISTS Users
+QUERY_USERS_INIT = '''CREATE TABLE IF NOT EXISTS Users
     (id integer primary key AUTOINCREMENT unique not NULL,
     username varchar(30) not NULL,
     password varchar(50) not NULL);'''
 
-users_query_fill = '''INSERT INTO Users (username, password)
+QUERY_USERS_FILL = '''INSERT INTO Users (username, password)
                         VALUES
                         ("Martin Angelov", "****"),
                         ("Ivo Donchev", "****"),
                         ("Radoslav Georgiev", "****"),
                         ("Rositza Zlateva", "****");'''
 
-reservations_query_init = '''CREATE TABLE IF NOT EXISTS Reservations
+QUERY_RESERVATIONS_INIT = '''CREATE TABLE IF NOT EXISTS Reservations
     (id integer primary key AUTOINCREMENT unique not NULL,
     user_id integer not NULL,
     projection_id integer not NULL,
@@ -48,7 +48,7 @@ reservations_query_init = '''CREATE TABLE IF NOT EXISTS Reservations
     FOREIGN KEY (user_id) REFERENCES Users(id),
     FOREIGN KEY (projection_id) REFERENCES Projections(id));'''
 
-reservations_query_fill = '''INSERT INTO Reservations (user_id, projection_id, row, col)
+QUERY_RESERVATIONS_FILL = '''INSERT INTO Reservations (user_id, projection_id, row, col)
                         VALUES
                         (3, 1, 2, 1),
                         (3, 1, 3, 5),
