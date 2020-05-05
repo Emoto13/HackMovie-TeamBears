316addb5549b9adcca3a802c3dd598935a5b1e0f
Emoto13 <emcho_spassov@abv.bg>
2020-05-05 11:06:45 +0300

    Remove fill queries

--- create_initial_database.py
+++ create_initial_database.py
@@ -6,19 +6,15 @@ from queries_initial_database import movie_query_init, movie_query_fill, project
 def main():
     queries = [
         movie_query_init,
-        movie_query_fill,
         projections_query_init,
-        projections_query_fill,
         users_query_init,
-        users_query_fill,
         reservations_query_init,
-        reservations_query_fill
     ]
 
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
-    for querie in queries:
-        cursor.execute(querie)
+    for query in queries:
+        cursor.execute(query)
     connection.commit()
     connection.close()
 
