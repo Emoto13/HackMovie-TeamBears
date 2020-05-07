ab28562a8edc36fc35a61ffbf5d0a4158ded53d0
Emoto13 <emcho_spassov@abv.bg>
2020-05-07 16:29:05 +0300

    Add introduction text

---
+++ utils/constants/introduction.py
@@ -0,0 +1,15 @@
+INTRODUCTION = ''' 
+Hello, 
+this is the world famous HackCinema.
+We are like IMDb, but better. ;D
+Here you can do a whole lot - check what movies and projections are available,
+reserve tickets for the hottest projections,
+explore the cinematic world  
+and so much more.
+We are happy to have you,
+
+Sincerely yours, HackCinema.
+
+P.S. For more information type help.
+There you will see everything you need to know for how this application.
+'''
