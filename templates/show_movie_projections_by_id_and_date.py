b00985db38a79da916f99247f6fb3a9647920154
Emoto13 <emcho_spassov@abv.bg>
2020-05-07 15:15:13 +0300

    Refactor show_movie_projections

--- templates/show_movie_projections_by_id_and_date.py
+++ templates/show_movie_projections_by_id_and_date.py
@@ -1,3 +1,9 @@
+def display_projections(projections, date):
+    if date is None:
+        return display_movie_projections(projections)
+    return display_movie_projections_for_specific_date(projections, date)
+
+
 def display_movie_projections_for_specific_date(projections, date):
     movie_name = projections[0][0]
     print(f"Projections for movie '{movie_name}' on date {date}: ")
