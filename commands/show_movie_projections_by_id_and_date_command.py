14cf032836d4db09df95ce0b27dbf680d47ddd47
Emoto13 <emcho_spassov@abv.bg>
2020-05-07 15:14:19 +0300

    Refactor show_movie_projections

--- commands/show_movie_projections_by_id_and_date_command.py
+++ commands/show_movie_projections_by_id_and_date_command.py
@@ -1,15 +1,9 @@
 from gateway.show_movie_projections_by_id_and_date import show_movies_by_id_or_date
-from templates.movie_projections_by_id_and_date import display_movie_projections_for_specific_date, \
-    display_movie_projections
+from templates.movie_projections_by_id_and_date import display_projections
+from verification.show_movie_projections_by_id_and_date import verify_projections
 
 
 def show_movie_projections_by_id_and_date(movie_id, date=None):
     projections = show_movies_by_id_or_date(movie_id, date)
-    if len(projections) == 0:
-        raise ValueError('No projections for this movie')
-    show_projections_wrapper(projections, date)
-
-def show_projections_wrapper(projections, date):
-    if date is None:
-        return display_movie_projections(projections)
-    return display_movie_projections_for_specific_date(projections, date)
+    verify_projections(projections)
+    display_projections(projections, date)
--- gateway/show_movie_projections_by_id_and_date.py
+++ gateway/show_movie_projections_by_id_and_date.py
@@ -1,18 +1,18 @@
 import sqlite3
 from utils.database_communication import DataBaseCommunication
-from utils.constants.queries_show_movie_projections_by_id_and_date import HOW_PROJECTIONS_BY_MOVIE_ID,\
+from utils.constants.queries_show_movie_projections_by_id_and_date import SHOW_PROJECTIONS_BY_MOVIE_ID,\
     SHOW_PROJECTIONS_BY_MOVIE_ID_AND_DATE
 
 
 def show_movies_by_id_or_date(movie_id, date):
     if date is None:
-        show_movies_by_id(movie_id)
-    show_movies_by_id_and_date(movie_id, date)
+        return show_movies_by_id(movie_id)
+    return show_movies_by_id_and_date(movie_id, date)
 
 
 def show_movies_by_id(movie_id):
     connection = sqlite3.connect('database.db')
-    movies = DataBaseCommunication.get_data(connection, HOW_PROJECTIONS_BY_MOVIE_ID, movie_id)
+    movies = DataBaseCommunication.get_data(connection, SHOW_PROJECTIONS_BY_MOVIE_ID, movie_id)
     return movies
 
 
