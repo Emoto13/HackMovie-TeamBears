77f9656f72d30c4d70cd8428431b25434753ed31
Nikolai Kasev <n.kasev98@gmail.com>
2020-05-08 16:14:56 +0300

    Refactored names

--- commands/show_reservations.py
+++ commands/show_reservations.py
@@ -2,6 +2,6 @@ from gateway.show_reservations import get_reservations
 from views.templates.show_reservations import display_reservations
 
 
-def show_reservations(name):
-    reservations = get_reservations(name)
+def show_reservations(user_name):
+    reservations = get_reservations(user_name)
     display_reservations(reservations)
--- utils/decorators/log_in_decorators.py
+++ utils/decorators/log_in_decorators.py
@@ -1,6 +1,6 @@
 def already_logged_in(func):
     def factory_wrapper(factory):
         if factory.is_logged_in:
-            raise ValueError('You are already logged in?!')
+            raise ValueError('You cannot execute this while logged in')
         func(factory)
     return factory_wrapper
