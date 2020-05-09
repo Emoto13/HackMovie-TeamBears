a712c8cb66a7f843588bb0037a54377751647a81
Emoto13 <emcho_spassov@abv.bg>
2020-05-09 16:43:44 +0300

    Update Update make_reservation_display

--- views/templates/make_reservation_display.py
+++ views/templates/make_reservation_display.py
@@ -1,42 +1,28 @@
-def greet_user(name):
-    print(f'Hello, {name}')
+def greet_user(user_name):
+    print(f'Hello, {user_name}')
 
 
 def display_movies_with_available_seats(movies):
-    print()
     print('Current movies: ')
     for movie in movies:
-        print(f'[{movie[0]}] - {movie[1]} {movie[2]}')
-    print()
+        print(movie)
 
 
 def display_projections(projections):
-    movie_name = projections[0][1]
-    print(f"\nProjections for movie '{movie_name}': ")
+    movie_name = projections[0].movie_name
+    print(f"Projections for movie '{movie_name}': ")
     for projection in projections:
-        projection_id, name, date, time, projection_type, seats = projection
-        print(f'[{projection_id}] - {date} {time} ({projection_type}) {seats} seats left')
-    print()
+        print(projection)
 
 
 def display_saloon_with_taken_seats(saloon):
-    print()
     for row in saloon:
         print("".join(row))
-    print(f'{(" " * (len(saloon)))}SCREEN IS HERE{(" " * (len(saloon)))}')
-    print()
 
 
-def display_reservation_info(seats, *projection_info):
-    name, rating, date, time, type_of_projection = projection_info
-    seats = [str(seat) for seat in seats]
-
-    print()
+def display_reservation_info(reservation):
     print('This is your reservation: ')
-    print(f"Movie: {name} ({rating})")
-    print(f"Date and time: {date} {time} ({type_of_projection})")
-    print(f"Seats: {' '.join(seats)}")
-    print()
+    print(reservation)
 
 
 def display_successful_reservation():
