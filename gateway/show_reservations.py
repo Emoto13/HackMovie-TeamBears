b89194019639b3ab1d0fbb41650e280fc08c0236
Emoto13 <emcho_spassov@abv.bg>
2020-05-09 19:27:34 +0300

    Queries renaming

--- gateway/show_reservations.py
+++ gateway/show_reservations.py
@@ -1,7 +1,7 @@
-from utils.constants.queries_show_reservations import SHOW_RESERVATIONS
+from utils.constants.queries_show_reservations import GET_RESERVATIONS
 from utils.database_communication import DataBaseCommunication
 
 
 def get_reservations(name):
-    sorted_movies = DataBaseCommunication.get_entries(SHOW_RESERVATIONS, name)
+    sorted_movies = DataBaseCommunication.get_entries(GET_RESERVATIONS, name)
     return sorted_movies
