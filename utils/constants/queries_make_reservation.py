258bae82d886b4955381f700dbe274f756b59a9d
Emoto13 <emcho_spassov@abv.bg>
2020-05-09 16:45:54 +0300

    Rename constants

--- utils/constants/queries_make_reservation.py
+++ utils/constants/queries_make_reservation.py
@@ -3,7 +3,7 @@ QUERY_MOVIES_WITH_AVAILABLE_SEATS = '''SELECT id, name, rating
                                        '''
 
 QUERY_GET_PROJECTIONS_BY_ID = \
-    '''SELECT Projections.id, Movies.name, date, time, type, (100 - COUNT(Reservations.projection_id)) as places_left
+    '''SELECT Projections.id as id, name, date, time, type, (100 - COUNT(Reservations.projection_id)) as seats_left
        FROM Movies 
        LEFT JOIN Projections ON Movies.id = Projections.movie_id 
        LEFT JOIN Reservations ON Projections.id = Reservations.projection_id
@@ -16,10 +16,10 @@ QUERY_GET_PROJECTION_INFO = '''SELECT name, rating, date, time, type
                                WHERE Projections.id = ?'''
 
 QUERY_GET_TAKEN_SEATS_BY_ROWS_AND_COLUMNS = '''SELECT row, col
-                                            FROM Projections
-                                            JOIN Reservations ON Projections.id = Reservations.projection_id
-                                            WHERE projection_id = ?
-                                            ORDER BY row;'''
+                                               FROM Projections
+                                               JOIN Reservations ON Projections.id = Reservations.projection_id
+                                               WHERE projection_id = ?
+                                               ORDER BY row;'''
 
 QUERY_GET_USER_ID_BY_USERNAME = '''SELECT id
                                     FROM Users 
