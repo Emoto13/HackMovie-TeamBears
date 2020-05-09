216abd1c0fff1828ab0b144c65bcdbc0c518c018
Emoto13 <emcho_spassov@abv.bg>
2020-05-09 16:45:22 +0300

    Update show_movie_projections templates

--- views/templates/show_movie_projections_by_id_and_date.py
+++ views/templates/show_movie_projections_by_id_and_date.py
@@ -5,14 +5,14 @@ def display_projections(projections, date):
 
 
 def display_movie_projections_for_specific_date(projections, date):
-    movie_name = projections[0][0]
+    movie_name = projections[0].name
     print(f"Projections for movie '{movie_name}' on date {date}: ")
     for projection in projections:
-        print(f"[{projection[1]}] - {projection[2]} ({projection[3]})")
+        print(projection)
 
 
 def display_movie_projections(projections):
-    movie_name = projections[0][0]
+    movie_name = projections[0].name
     print(f"Projections for movie '{movie_name}': ")
     for projection in projections:
-        print(f"[{projection[1]}] - {projection[2]} {projection[3]} ({projection[4]})")
+        print(projection)
