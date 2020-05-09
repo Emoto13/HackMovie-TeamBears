0ef1e6758ea4e0cc305e0c5c4f26d977a048fc8e
Emoto13 <emcho_spassov@abv.bg>
2020-05-09 13:44:49 +0300

    Add DBCommunication.get_single_entry()

--- utils/database_communication.py
+++ utils/database_communication.py
@@ -3,11 +3,26 @@ import sqlite3
 
 class DataBaseCommunication:
     @staticmethod
-    def get_data(query, *arguments):
+    def get_single_entry(query, *arguments):
         connection = sqlite3.connect('database.db')
         args_for_query = [arg for arg in arguments]
         try:
             with connection:
+                connection.row_factory = sqlite3.Row
+                cursor = connection.cursor()
+                cursor.execute(query, args_for_query)
+                result = cursor.fetchone()
+                return result
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def get_entries(query, *arguments):
+        connection = sqlite3.connect('database.db')
+        args_for_query = [arg for arg in arguments]
+        try:
+            with connection:
+                connection.row_factory = sqlite3.Row
                 cursor = connection.cursor()
                 cursor.execute(query, args_for_query)
                 result = cursor.fetchall()
