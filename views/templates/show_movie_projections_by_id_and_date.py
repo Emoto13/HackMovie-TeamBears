285ea02fb98f26a272e8092f16ec174a1d3224f8
Emoto13 <emcho_spassov@abv.bg>
2020-05-12 13:43:09 +0300

    Update templates

--- views/templates/show_movie_projections_by_id_and_date.py
+++ views/templates/show_movie_projections_by_id_and_date.py
@@ -2,5 +2,5 @@ def display_projections(projections, date):
     movie_name = projections[0].movie_name
     print(f"Projections for movie '{movie_name}' {'on date ' + date if date else ''}")
     for projection in projections:
-        print(f"[{projection.projection_id}] - {date + ' ' if date else ''}{projection.time}"
+        print(f"[{projection.projection_id}] - {projection.date + ' ' if not date else ''}{projection.time}"
               f" {projection.projection_type}")
--- views/templates/show_reservations.py
+++ views/templates/show_reservations.py
@@ -2,5 +2,5 @@ def display_reservations(reservations):
     print('Your current reservations are:')
     for reservation in reservations:
         print(f'[{reservation.reservation_id}] {reservation.movie_name} on date {reservation.date} '
-              f'at {reservation.time} ({reservation.projection_type})')
-        print(f'Seat: {reservation.row} {reservation.col}')
+              f'at {reservation.time} ({reservation.projection_type}) '
+              f'Seat: {reservation.row} {reservation.col}')
