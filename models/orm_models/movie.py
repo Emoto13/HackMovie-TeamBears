d2f267064120c35b383cfadf50d91e8702dbcb2d
Emoto13 <emcho_spassov@abv.bg>
2020-05-14 12:18:46 +0300

    Add orm classes

---
+++ models/orm_models/movie.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from models.orm_models.base import Base
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+    movie_id = Column(Integer, primary_key=True)
+    movie_name = Column(String)
+    movie_rating = Column(Float)
+
+    projections = relationship("Projection", backref="movie")
---
+++ models/orm_models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from models.orm_models.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String)
+    user_password = Column(String)
