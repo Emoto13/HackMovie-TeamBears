f215f57ca6f85779c320faa2eadfef7b3199d80e
Nikolai Kasev <n.kasev98@gmail.com>
2020-05-14 18:00:46 +0300

    Refactored and added ORM logic to sign_up

---
+++ views/templates/sign_up_display.py
@@ -0,0 +1,18 @@
+def display_incorrect_verification_password():
+    print('Passwords do not match')
+
+
+def display_incorrect_password_length():
+    print('Password should be at least 8 characters')
+
+
+def display_incorrect_uppercase_format():
+    print('Password should have at least 1 uppercase char')
+
+
+def display_incorrect_special_char_format():
+    print('Password should have at least 1 special char')
+
+
+def display_user_name_is_taken():
+    print('User_name is taken!')
