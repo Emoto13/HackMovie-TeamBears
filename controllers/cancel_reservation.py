edcc21ca4138374009c14d2a59e846bbc28794d6
Emoto13 <emcho_spassov@abv.bg>
2020-05-15 08:26:28 +0300

    Refactor cancel_reservation

--- controllers/cancel_reservation.py
+++ controllers/cancel_reservation.py
@@ -1,15 +1,15 @@
 from views.templates.cancel_reservation_display import display_cancel_reservation
 from views.templates.cancel_reservation_input import get_reservation
-from gateway.cancel_reservation import remove_reservation, get_all_reservations_by_user_name
+from gateway.cancel_reservation import remove_reservation, get_all_reservations_by_user_id
 
 
-def cancel_reservation(user_name):
-    all_reservations = get_reservations(user_name)
-    reservation_id = get_reservation(all_reservations)
+def cancel_reservation(user_id):
+    reservation_ids = get_reservations_ids(user_id)
+    reservation_id = get_reservation(reservation_ids)
     remove_reservation(reservation_id)
     display_cancel_reservation()
 
 
-def get_reservations(user_name):
-    all_reservations = get_all_reservations_by_user_name(user_name)
-    return list(map(lambda reservation: reservation['reservation_id'], all_reservations))
+def get_reservations_ids(user_id):
+    reservations = get_all_reservations_by_user_id(user_id)
+    return list(map(lambda reservation: reservation.reservation_id, reservations))
